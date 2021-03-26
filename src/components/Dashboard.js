34029b34fbf5edc94712b5b8e25c9d62c45539bd
Sandra To <sandra.to.e@gmail.com>
2021-03-25 19:54:51 -0700

    Fetch data from schedule-api and synchronize localStorage

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,39 +1,80 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import classnames from "classnames";
 import Loading from "components/Loading";
 import Panel from "components/Panel";
 
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "helpers/selectors";
+
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue:getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
 class Dashboard extends Component {
 
+  
   state = {
-    loading: false,
-    focused: null
+    loading: true,
+    focused: null,
+    days : [],
+    appointments : {},
+    interviewers : {}
   };
   
+  // Lifecycle phase: Mount
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    };
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
+  };
+
+  // Lifecycle phase: Update
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  };
+
   /* Instance Method */
   selectPanel(id) {
     this.setState(previousState => ({
@@ -65,9 +106,8 @@ class Dashboard extends Component {
       .map(panel => (
         <Panel 
           key={panel.id}
-          id={panel.id}
           label={panel.label}
-          value={panel.value}
+          value={panel.getValue(this.state)}
           // bind with arrow function
           onSelect={event => this.selectPanel(panel.id)}
         />
